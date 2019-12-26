f91628c43aa7f70cf60211d964a8567f231845af
vityareabko <reabko15@gmail.com>
2019-12-26 23:26:45 +0200

    scroll, fixed errors nav, add section relax room, andrename saction relax_room on the reviews

--- js/main.js
+++ js/main.js
@@ -1,66 +1,58 @@
-$("#navToggle").click(function() {
-    $(this).toggleClass("active");
-    $(".overlay").toggleClass("open");
-    // this line ▼ prevents content scroll-behind
-    $("body").toggleClass("locked"); 
-});
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     autoplay:true,
-//     autoplayTimeout: 2000,
-//     margin:80,
-//     nav:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:4
-//         }
-//     }
-// });
 
-// $('.carousel').owlCarousel({
-//     loop:true,
-//     autoplay:true,
-//     autoplayTimeout: 2000,
-//     margin:80,
-//     nav:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:4
-//         }
-//     }
-// });
 
-$(function(){
-    $('.owl-carousel').each(function(){
-      $(this).owlCarousel({
-        loop:true,
-        autoplay:true,
-        autoplayTimeout: 2000,
-        margin:80,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
-      });
+
+
+  
+$(document).ready(function(){
+    $("#navToggle").click(function() {
+        $("#navToggle").toggleClass("active");
+        $(".overlay").toggleClass("open");
+        $("body").toggleClass("locked"); 
+        
     });
-  });
+    $(".menu__item").on("click", function(){
+        $("#navToggle").toggleClass("active");
+        $(".overlay").toggleClass("open");
+        $("body").toggleClass("locked"); 
+    });
+
+
+
+    $(function(){
+        $('.owl-carousel').each(function(){
+          $(this).owlCarousel({
+            loop:true,
+            autoplay:true,
+            autoplayTimeout: 2000,
+            margin:80,
+            nav:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+          });
+        });
+      });
+
+      
+    $("#menu").on("click","a", function (event) {  
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+});
+
+
